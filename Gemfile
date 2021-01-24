fd75937a3d121bdc324b002e5c25d7d92abfa44f
nimuC <nimuC@outlook.com>
2021-01-24 21:54:08 +0900

    トピックをredisに保存するようにした

--- Gemfile
+++ Gemfile
@@ -8,5 +8,6 @@ git_source(:github) {|repo_name| 'https://github.com/#{repo_name}' }
 
 gem 'discordrb'
 gem 'dotenv'
+gem 'redis'
 gem 'rspec'
 gem 'rubocop'
--- main.rb
+++ main.rb
@@ -3,12 +3,19 @@
 require 'bundler/setup'
 require 'discordrb'
 require 'dotenv'
+require 'redis'
+
 Dotenv.load
+
+redis = Redis.new(url: ENV['REDIS_URL'])
 
 bot = Discordrb::Bot.new token: ENV['TEACHER_TOKEN']
 
 bot.message(contains: /\?set .+/) do |event|
   topic = event.content.delete_prefix('?set ')
+
+  redis.set(event.channel.id.to_s, topic)
+
   event.server.general_channel.send_message("#{event.channel.name} では #{topic} について話しています")
 end
 
